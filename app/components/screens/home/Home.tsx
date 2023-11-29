876ceaa100f4bdbfe8f943de15a9143bd3276b1d
KonstantinKaz <kaz.world.work@mail.ru>
2023-11-29 20:03:05 +0300

    хз что нового

--- app/components/screens/home/Home.tsx
+++ app/components/screens/home/Home.tsx
@@ -1,20 +1,32 @@
-import Layout from '@/components/layout/Layout'
 import { Table } from 'antd'
-import { FC } from 'react'
+import axios from 'axios'
+import { FC, useEffect } from 'react'
 import { columns } from './table/TableColumns'
 import { data } from './table/TableData'
+import Layout from '@/components/layout/Layout'
+
+const token =
+	'O5%2FYViaep7fV915GhvNxO5aLFCbbTFhYlezXjfzY3kPxMTZgPEFWacFk%2BbO2lmIVpB4FZl3gw4Gl4vqwmhv0ZgOTk7d1eWKMJ3aGRUdl304lDqTogWdO58YrPKXM%2B7Jen7J1tsNJ8an0pVaM3JB9lLdC7YCPDTNtDjFVPUCL0Ks%3D'
+
+const baseUrl = 'https://e.mospolytech.ru/old/lk_api.php/'
 
 const Home: FC = () => {
-	// return (
-	// 	<Layout>
-	// 		<Table
-	// 			columns={columns}
-	// 			dataSource={data}
-	// 			pagination={{ pageSize: 10 }}
-	// 		/>
-	// 	</Layout>
-	// )
+	// const getStudents = async (page: string, perPage: string) => {
+	// 	const response = await axios.get(
+	// 		baseUrl +
+	// 			`?getStudents&page=${page}
+	// 	&perpage=${perPage}
+	// 	&token=${token}`
+	// 	)
 
+	// 	return response.data
+	// }
+	// useEffect(() => (), [])
+	// функция выполняется, когда [] пуст
+
+	// useEffect(() => {
+	// 	console.log(getStudents('1', '50'))
+	// }, [])
 	return (
 		<Layout>
 			{data.length > 0 ? (
--- next.config.js
+++ next.config.js
@@ -1,3 +1,29 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+// 	poweredByHeader: false,
+// 	optimizeFonts: false,
+// 	env: {
+// 		APP_URL: process.env.REACT_APP_URL,
+// 		APP_ENV: process.env.REACT_APP_ENV,
+// 		SERVER_URL: process.env.SERVER_URL,
+// 	},
+// 	async rewrites() {
+// 		return [
+// 			{
+// 				source: '/api/:path*',
+// 				destination: 'http://localhost:4200/api/:path*',
+
+// 			},
+// 			{
+// 				source: '/uploads/:path*',
+// 				destination: 'http://localhost:4200/uploads/:path*',
+// 			},
+// 		]
+// 	},
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	poweredByHeader: false,
@@ -12,13 +38,15 @@ const nextConfig = {
 			{
 				source: '/api/:path*',
 				destination: 'http://localhost:4200/api/:path*',
-
 			},
 			{
 				source: '/uploads/:path*',
 				destination: 'http://localhost:4200/uploads/:path*',
 			},
 		]
+	},
+	images: {
+		domains: ['via.placeholder.com', 'localhost:4200'],
 	},
 }
 
