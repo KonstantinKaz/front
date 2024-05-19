6c2018f3e90e037c1f774ae22b467b5b06369831
KonstantinKaz <kaz.world.work@mail.ru>
2024-05-19 11:45:51 +0300

    lab 3

--- pages/pagination.tsx
+++ pages/pagination.tsx
@@ -1,8 +1,8 @@
-import Layout from '@/components/layout/Layout'
-import PhotoGallery from '@/components/screens/Pagination/PhotoGallery'
-import fetchPhotos from '@/utils/fetchPhotos'
+import Layout from "@/components/layout/Layout"
+import PhotoGallery from "@/components/screens/Pagination/PhotoGallery"
+import fetchPhotos from "@/utils/fetchPhotos"
 
-import { FC } from 'react'
+import { FC } from "react"
 
 export async function getServerSideProps() {
 	const initialPhotos = await fetchPhotos(1)
